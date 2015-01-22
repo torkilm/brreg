f2a2cfd00e400153c2d0032774501f751a0ff199
torkilm <torkil@heimdalit.no>
2015-01-22 20:01:40 +0100

    Div

--- app.js
+++ app.js
@@ -1,17 +1,24 @@
-var app = angular.module('myApp', ['ngResource']);
+var app = angular.module('myApp', ['ngResource','ui.bootstrap']);
 
 app.controller('brreg', ['$scope', 'Enhetsregisteret', 
   function ($scope, ereg) {
-    $scope.searchText = 'Search here...';
+    $scope.Side = 1;
 
     $scope.Search = function () {
-      ereg.get({query:$scope.searchText}, function (data) {
+      ereg.get({query:$scope.searchText,page:$scope.Side}, function (data) {
         $scope.Resultat = data.entries;
         $scope.Antall = data.posts;
+        $scope.Side = data.page;
+     
+        $scope.Sider = data.pages;
         console.log(data);
       });
     }; // End Search
+    $scope.pageChanged = function () {
+      console.log($scope.Side);
+      $scope.Search();
 
+    };
   }]); // End controller
 
 app.factory('Enhetsregisteret', ['$resource', 
